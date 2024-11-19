76dac274a20006d7816f36cee23da89f11129838
Palior <plizama.orostica@gmail.com>
2024-11-18 23:04:42 -0300

    added background change button

---
+++ file.js
@@ -0,0 +1,8 @@
+// Seleccionar el botón por su ID
+const boton = document.getElementById("btnCambiarFondo");
+
+// Agregar un evento al botón
+boton.addEventListener("click", () => {
+  // Cambiar el color de fondo
+  document.body.style.backgroundColor = "lightblue";
+});
